e1483904908db448ea88d09d8342b6ab7806570e
Luis Antonio Miranda <luismiranda.aprog@gmail.com>
2024-05-24 00:37:00 -0400

    Despliegue menu seleccionable para registrar la salida de proveedores registrados

--- src/app/prov-salida/prov-salida.page.ts
+++ src/app/prov-salida/prov-salida.page.ts
@@ -9,8 +9,10 @@ import { AlertController, NavController } from '@ionic/angular';
 })
 export class ProvSalidaPage implements OnInit {
   formularioProveedor: FormGroup;
+  proveedores: any[] = [];
+  proveedorSeleccionado: any = null;
 
-  constructor(private fb: FormBuilder, private navCtrl: NavController, /*Alertas*/ public alertController: AlertController) {
+  constructor(private fb: FormBuilder, private navCtrl: NavController, public alertController: AlertController) {
     this.formularioProveedor = this.fb.group({
       nombreProveedor: ['', Validators.required],
       patenteVehiculo: ['', Validators.required],
@@ -20,16 +22,38 @@ export class ProvSalidaPage implements OnInit {
     });
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.cargarProveedores();
+  }
+
+  cargarProveedores() {
+    const movimientos = JSON.parse(localStorage.getItem('movimiento_registro_proveedor') || '[]');
+    this.proveedores = movimientos;
+  }
+
+  seleccionarProveedor(event: any) {
+    const patenteVehiculo = event.target.value;
+    const nombreProveedor = event.target.value;
+
+    this.proveedorSeleccionado = this.proveedores.find(proveedor => proveedor.patenteVehiculo === patenteVehiculo);
+    if (this.proveedorSeleccionado) {
+      this.formularioProveedor.patchValue({
+        patenteVehiculo: this.proveedorSeleccionado.patenteVehiculo,
+        nombreProveedor: this.proveedorSeleccionado.nombreProveedor
+      });
+    }
+  }
 
   guardar() {
+    if (!this.proveedorSeleccionado) {
+      console.error('Proveedor no seleccionado');
+      return;
+    }
 
     let salida_proveedor = this.formularioProveedor.value;
-    // Combinar fecha y hora en un solo objeto Date
+
     let fechaSalida = new Date(salida_proveedor.fechaSalida);
     let horaSalida = new Date(salida_proveedor.horaSalida);
-
-    // Ajustar las horas, minutos y segundos de fechaSalida usando horaSalida
     fechaSalida.setHours(horaSalida.getHours(), horaSalida.getMinutes(), horaSalida.getSeconds());
 
     if (isNaN(fechaSalida.getTime())) {
@@ -38,17 +62,16 @@ export class ProvSalidaPage implements OnInit {
     }
 
     salida_proveedor.fechaSalida = fechaSalida.toISOString();
+    salida_proveedor.nombreProveedor = this.proveedorSeleccionado.nombreProveedor;
+    salida_proveedor.numeroOc = this.proveedorSeleccionado.numeroOc;
+    salida_proveedor.fechaIngreso = this.proveedorSeleccionado.fechaIngreso;
+    salida_proveedor.horaIngreso = this.proveedorSeleccionado.horaIngreso;
 
-    // Obtener el array existente de `salida_proveedor` o inicializar uno nuevo
     let movimientos = JSON.parse(localStorage.getItem('salida_proveedor') || '[]');
-    
-    // Agregar la nueva entrada al array
     movimientos.push(salida_proveedor);
-    
-    // Guardar el array actualizado en `localStorage`
     localStorage.setItem('salida_proveedor', JSON.stringify(movimientos));
-    
-    console.log('Formulario:', salida_proveedor); // Debug: Verificar que los datos se están recogiendo
+
+    console.log('Formulario:', salida_proveedor);
     this.navCtrl.navigateRoot('salida-exitoso');
   }
 }
